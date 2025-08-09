d502fbf25ebac1cb0c811f55ed2cbe234bbba04d
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-08-09 22:01:19 +0300

    добавила типизацию в filterUsers, переименовала UserCard

--- src/components/FilterUsers.vue
+++ src/components/FilterUsers.vue
@@ -24,14 +24,16 @@ import { ref } from 'vue';
 import AppButton from '@/components/ui/AppButton.vue';
 import AppInputText from '@/components/ui/AppInputText.vue';
 
+interface Props {
+  inputValue: string,
+}
+interface Emits {
+  (event: 'filterUsers', params: FilterParams):void,
+}
+
 const inputValueLocal = ref('');
-defineProps({
-  inputValue: {
-    type: String,
-    required: true,
-  }
-})
-const emits = defineEmits(['filterUsers']);
+defineProps<Props>();
+const emits = defineEmits<Emits>();
 const buttonList = ref ([
   { id: 1, text: 'Все пользователи', isActive: true, color: 'blue', role: 'all' },
   { id: 2, text: 'Админы', isActive: false, color: 'pink', role: 'admin' },
