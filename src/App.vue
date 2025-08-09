664d8169021abad2df58ba4ab792bd9bd7c271bd
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-08-09 19:26:14 +0300

    отрисовала карточки юзеров циклом

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,32 @@
         inputValue=""
       />
     </div>
-    <UserIcon />
+    <UserCard 
+      :userlist="userListDefault[0]"
+      v-for="user in userListDefault"
+      :key="user.id"
+      :user="user"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import SetUsers from '@/components/SetUsers.vue';
-import UserIcon from '@/components/ui/UserIcon.vue';
+import UserCard from '@/components/ui/UserCard.vue';
+// import { ref } from 'process';
 
+const userListDefault = [
+  { id: 1001, name: 'Ваня', role: 'user' },
+  { id: 1002, name: 'Ваня', role: 'admin' },
+  { id: 1003, name: 'Петя', role: 'admin' },
+  { id: 1004, name: 'Евкакий', role: 'editor' },
+  { id: 1005, name: 'Пупа', role: 'user' },
+  { id: 1006, name: 'Лупа', role: 'user' }
+];
+// const filters = ref({
+//   role: 'all',
+//   query: '',
+// })
 
 </script>
 
